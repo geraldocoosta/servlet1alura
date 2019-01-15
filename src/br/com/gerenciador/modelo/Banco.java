56a03e5341102815db512194d67adbe7eceeb9e0
Geraldo <geraldocoosta@gmail.com>
2019-01-15 10:04:13 -0200

    primeiro contato com jsp no curso

--- src/br/com/gerenciador/modelo/Banco.java
+++ src/br/com/gerenciador/modelo/Banco.java
@@ -1,14 +1,25 @@
-package br.com.gerenciador.servlet;
+package br.com.gerenciador.modelo;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import br.com.gerenciador.modelo.Empresa;
 
 public class Banco {
 	
 	private static List<Empresa> listaEmpresas = new ArrayList<>();
 
+	static {
+		Empresa empresa = new Empresa();
+		empresa.setId(0);
+		empresa.setNome("Caixa");
+		Empresa empresa2 = new Empresa();
+		empresa2.setId(1);
+		empresa2.setNome("Caelum");
+		
+		listaEmpresas.add(empresa);
+		listaEmpresas.add(empresa2);
+		
+	}
+	
 	public void adiciona(Empresa empresa) {
 		Banco.listaEmpresas.add(empresa);
 	}
